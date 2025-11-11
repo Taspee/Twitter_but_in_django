2b9080123710cd5a7677ec99234b9431fb83487c
taspee <tfaspee@miuandes.cl>
2025-11-11 00:40:05 -0300

    signup funcional

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -5,4 +5,7 @@ def login_view(request):
     return render(request, 'login.html')
 #home view
 def home_view(request):
+    
     return render(request, 'home.html')
+def signup_view(request):
+    return render(request, 'signup.html')
